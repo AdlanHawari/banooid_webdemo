43835335821a13dd076f9df80d86e0da35da7df7
Adlan Hawari <hwr.adlan@gmail.com>
2021-07-14 15:26:25 +0700

    hashrouter in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 // import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
